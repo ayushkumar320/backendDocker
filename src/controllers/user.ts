0657abe2e373a7049af56b08b96a733cb594bfc2
Ayush <ayushkumar85385@gmail.com>
2025-10-21 10:59:12 +0530

    added types to login and signup

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -10,11 +10,19 @@ const JWT_SECRET = process.env.JWT_SECRET;
 interface SignupRequestBody {
   email: string;
   firstName: string;
-  lastName: string;
+  lastName?: string;
   password: string;
 }
 
-export async function userSignup(req: Request, res: Response) {
+interface LoginRequestBody {
+  email: string;
+  password: string;
+}
+
+export async function userSignup(
+  req: Request<never, any, SignupRequestBody>,
+  res: Response
+) {
   const {email, firstName, lastName, password} = req.body;
   try {
     const existingUser = await prisma.user.findUnique({where: {email}});
@@ -23,16 +31,21 @@ export async function userSignup(req: Request, res: Response) {
         status: {
           code: 400,
           status: "Error",
-        }, 
+        },
         data: {
           message: "User with this email already exists, try logging in",
-        }
-      })
+        },
+      });
     } else {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
       const newUser = await prisma.user.create({
-        data: {email, firstName, lastName, password: hashedPassword},
+        data: {
+          email,
+          firstName,
+          lastName: lastName ?? null,
+          password: hashedPassword,
+        },
       });
       return res.json({
         status: {
@@ -60,12 +73,15 @@ export async function userSignup(req: Request, res: Response) {
       data: {
         message: "Internal server error",
         error: error,
-      }
-    })
+      },
+    });
   }
 }
 
-export async function userLogin(req: Request, res: Response) {
+export async function userLogin(
+  req: Request<never, any, LoginRequestBody>,
+  res: Response
+) {
   const {email, password} = req.body;
   try {
     const user = await prisma.user.findUnique({where: {email}});
@@ -74,11 +90,11 @@ export async function userLogin(req: Request, res: Response) {
         status: {
           code: 400,
           status: "Error",
-        }, 
+        },
         data: {
           message: "Invalid email or password",
-        }
-      })
+        },
+      });
     } else {
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (!isPasswordValid) {
@@ -89,14 +105,18 @@ export async function userLogin(req: Request, res: Response) {
           },
           data: {
             message: "Invalid password",
-          }
-        })
-      } else {
-        const token = jwt.sign({
-          id: user.id,
-        }, JWT_SECRET as string, {
-          expiresIn: "7d"
+          },
         });
+      } else {
+        const token = jwt.sign(
+          {
+            id: user.id,
+          },
+          JWT_SECRET as string,
+          {
+            expiresIn: "7d",
+          }
+        );
         return res.status(200).json({
           status: {
             code: 200,
@@ -105,8 +125,8 @@ export async function userLogin(req: Request, res: Response) {
           data: {
             message: "Login successful",
             token: token,
-          }
-        })
+          },
+        });
       }
     }
   } catch (error) {
@@ -118,7 +138,8 @@ export async function userLogin(req: Request, res: Response) {
       data: {
         message: "Internal server error",
         error: error,
-      }
-    })
+      },
+    });
   }
 }
+
